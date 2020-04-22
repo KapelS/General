99e5ed2e8dc1759ab926dc9aaae5bbc0c6f7e9e5
Vladislav-Oleksienko <splinfao1@gmail.com>
2020-04-22 16:52:55 +0300

    changed styles, added bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+const images = require.context('../images', true);
+const imagePath = (name) => images(name, true);
+
+import "../src/stylesheets/application"
+import "../src/javascript/application"
+import "bootstrap"
